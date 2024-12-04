99ecef10570e13db1fca903b3eb88daa52f5ad82
Nikolay Zenchenko <nik2704@yandex.ru>
2024-12-04 13:07:32 +0400

    doc

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['print_2',['print',['../utils_8h.html#a439abb8457285c7c4dd63132b716ba25',1,'utils.h']]],
+  ['print_5fip_3',['print_ip',['../print__ip_8h.html#a0b38855cc83ebbf161482287b6b334a0',1,'print_ip(const T &amp;value):&#160;print_ip.h'],['../print__ip_8h.html#a1da07421b4cd2e37e1bcadaeaf81d7d1',1,'print_ip(const T &amp;value):&#160;print_ip.h'],['../print__ip_8h.html#ae9d10152f3bc7ad28fb8db20cf165643',1,'print_ip(const T &amp;container):&#160;print_ip.h']]],
+  ['print_5fip_2eh_4',['print_ip.h',['../print__ip_8h.html',1,'']]],
+  ['print_5ftuple_5',['print_tuple',['../print__ip_8h.html#a077c37bffa90b536db277f42c92b4658',1,'print_ip.h']]]
+];
---
+++ docs/html/search/functions_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['print_14',['print',['../utils_8h.html#a439abb8457285c7c4dd63132b716ba25',1,'utils.h']]],
+  ['print_5fip_15',['print_ip',['../print__ip_8h.html#a0b38855cc83ebbf161482287b6b334a0',1,'print_ip(const T &amp;value):&#160;print_ip.h'],['../print__ip_8h.html#a1da07421b4cd2e37e1bcadaeaf81d7d1',1,'print_ip(const T &amp;value):&#160;print_ip.h'],['../print__ip_8h.html#ae9d10152f3bc7ad28fb8db20cf165643',1,'print_ip(const T &amp;container):&#160;print_ip.h']]],
+  ['print_5ftuple_16',['print_tuple',['../print__ip_8h.html#a077c37bffa90b536db277f42c92b4658',1,'print_ip.h']]]
+];
