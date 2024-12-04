47c9cfbe7610d781840a13319db1c7fa545ba070
Nikolay Zenchenko <nik2704@yandex.ru>
2024-12-04 12:53:27 +0400

    first commit

--- print_ip.h
+++ print_ip.h
@@ -1,3 +1,10 @@
+/*!
+\file
+\brief Файл, содержащий шаблонную функцию печати адресов, созданную с использованием механизма SFINAE
+
+Данный файл содержит в себе набор функций для разных типов данных
+*/
+
 #include <type_traits>
 #include <vector>
 #include <list>
@@ -6,7 +13,10 @@
 #include "utils.h"
 
 
-// 1. целые числа
+/*!
+Шаблонная функция обработки целочисленных значений
+\param[in] value значение целого типа
+*/
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value>::type
 print_ip(const T& value) {
@@ -25,26 +35,38 @@ print_ip(const T& value) {
     std::cout << '\n';
 }
 
-// 2. строки
+/*!
+Шаблонная функция обработки строковых значений
+\param[in] value значение строкового типа
+*/
 template <typename T>
 typename std::enable_if<std::is_same<T, std::string>::value>::type
 print_ip(const T& value) {
     std::cout << value << '\n';
 }
 
-// 3. контейнеры
-template <typename T>
+/*!
+Шаблонная функция обработки контейнеров
+\param[in] value значение контейнерного типа
+*/template <typename T>
 typename std::enable_if<!std::is_same<T, std::string>::value && std::is_same<typename T::value_type, typename T::value_type>::value>::type
 print_ip(const T& container) {
     print(container.begin(), container.end());
     std::cout << '\n';
 }
 
-// 4. tuple
+/*!
+Шаблонная функция обработки tuple - выход из рекурсии
+\param[in] value значение типа tuple
+*/
 template <typename Tuple, size_t Index = 0>
 typename std::enable_if<Index == std::tuple_size<Tuple>::value>::type
 print_tuple(const Tuple&) { }
 
+/*!
+Шаблонная функция обработки tuple - вывод элементов (рекурсивно)
+\param[in] value значение типа tuple
+*/
 template <typename Tuple, size_t Index = 0>
 typename std::enable_if<Index < std::tuple_size<Tuple>::value>::type
 print_tuple(const Tuple& tuple) {
@@ -55,6 +77,10 @@ print_tuple(const Tuple& tuple) {
     print_tuple<Tuple, Index + 1>(tuple);
 }
 
+/*!
+Шаблонная функция обработки tuple - начало работы, проверка условия, при котором все элементы должны быть одного типа
+\param[in] value значение типа tuple
+*/
 template <typename... Args>
 typename std::enable_if<std::conjunction<std::is_same<Args, typename std::tuple_element<0, std::tuple<Args...>>::type>...>::value>::type
 print_ip(const std::tuple<Args...>& tuple) {
--- utils.h
+++ utils.h
@@ -1,5 +1,18 @@
+/*!
+\file
+\brief Файл, содержащий вспомогательные функции
+
+Данный файл содержит в себе функции, используемые в программе
+*/
+
 #include <iostream>
 
+
+/*!
+Шаблонная функция печати значений по итераторам
+\param[in] begin Начальный итератор
+\param[in] end Конечный итератор
+*/
 template <typename Iterator>
 void print(Iterator begin, Iterator end) {
     if (begin == end) {
